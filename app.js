ef0788cc96d5d7de02a259fa4fe7669146fa5441
myleobot <myleobot@gmail.com>
2022-05-13 15:55:02 +0900

    add pm2

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ const { secretKey } = require('./config/secretKey')
 const routes = require('./router')
 
 const app = express()
-
 const bodyParser = require('body-parser')
+
+let isDisableKeepAlive = false
+app.use(function (req, res, next) {
+  if (isDisableKeepAlive) {
+    res.set('Connection', 'close')
+  }
+  next()
+})
+
 app.use(require('connect-history-api-fallback')())
 app.use(express.static('dist'))
 
@@ -27,7 +35,17 @@ app.use('/api/admin', routes)
 // app.use('/api/admin', adminRouter)
 
 const handleListening = () => {
+  if (process.send) {
+    process.send('ready')
+  }
   console.log('👀 server is listening on 3000 port🚀')
 }
-app.listen(3000, handleListening)
+const serverListener = app.listen(3000, handleListening)
+process.on('SIGINT', function () {
+  isDisableKeepAlive = true
+  serverListener.close(function (err) {
+    console.log('server closed')
+    process.exit(err ? 1 : 0)
+  })
+})
 module.exports = app
