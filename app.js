2ea1b958e5f77c7c56b8ad0d974aae4f66174f67
myleobot <myleobot@gmail.com>
2022-07-28 19:26:36 +0900

    다시 sse 설치

--- app.js
+++ app.js
@@ -7,9 +7,81 @@ const cors = require('cors')
 const checkToken = require('./middlewares/auth')
 const adminRouter = require('./router/admin')
 const routes = require('./router')
-
 const app = express()
 const bodyParser = require('body-parser')
+
+const mysql = require('mysql')
+const MySQLEvents = require('@rodrigogs/mysql-events')
+let timeStamp = null
+
+const program = async () => {
+  const connection = mysql.createConnection({
+    host: 'aaf2aamu92cvf4.cthuqtxb1hrh.ap-northeast-2.rds.amazonaws.com',
+    user: 'chiumdb',
+    password: 'cldna2021!',
+    database: 'chium',
+  })
+
+  const instance = new MySQLEvents(connection, {
+    startAtEnd: true,
+    excludedSchemas: {
+      mysql: true,
+    },
+  })
+
+  await instance.start()
+
+  instance.addTrigger({
+    name: 'JOB_LOG_AFTER_INSERT',
+    expression: '*',
+    statement: MySQLEvents.STATEMENTS.ALL,
+    onEvent: async (event) => {
+      console.log('res2>>>>>', event.table)
+      dbChanged = true
+      timeStamp = Math.floor(Date.now())
+      //   console.log('dbChanged in addTrigger>>>>>>', dbChanged)
+    },
+  })
+
+  instance.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error)
+  instance.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error)
+}
+
+const listener = async () => {
+  await program()
+    .then(() => {
+      console.log('Waiting for database events...')
+    })
+    .catch(console.error)
+}
+listener()
+
+const sendResponse = (res) => {
+  setTimeout(() => {
+    sendResponse(res)
+    // if (dbChanged) {
+    //   res.write('data: ' + dbChanged + `\n\n`)
+    //   console.log('after sendResponse', dbChanged)
+    // }
+    // res.write('data: ' + dbChanged + `\n\n`)
+    res.write('data: ' + timeStamp + `\n\n`)
+    dbChanged = false
+  }, 5000)
+}
+
+// let dbChangedOld = null
+app.get('/sse', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Credentials': true,
+    Connection: 'keep-alive',
+  })
+  sendResponse(res)
+})
 
 let isDisableKeepAlive = false
 app.use(function (req, res, next) {
@@ -32,7 +104,6 @@ app.use(cookieParser())
 app.use(cors())
 app.use(checkToken)
 app.use('/api/admin', routes)
-// app.use('/api/admin', adminRouter)
 
 const handleListening = () => {
   if (process.send) {
